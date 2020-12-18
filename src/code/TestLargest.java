0b0ba4f1657bbc7c1bf77810f63113773d9b5086
Antony Gordon <gordonad@umich.edu>
2020-12-18 16:16:38 -0500

    Add TestPositiveTest

---
+++ src/code/TestLargest.java
@@ -0,0 +1,30 @@
+package code;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+public class TestLargest {
+
+
+
+	@Test
+	public void testPositive() {
+        int[] arr = new int[4];
+        arr[0] = 11;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        Largest temp1 = new Largest();
+        int maxValue = temp1.largest(arr);
+        int expected = 11;
+        Assert.assertEquals("Values was not max", expected,  maxValue);
+        
+	/** add tests to check for this unit test **/	
+    }
+
+	
+	public void testNegative(){
+        /** add tests to check for this unit test **/ 
+	}
+
+}
