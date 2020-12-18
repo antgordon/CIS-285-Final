1e194b3f6fc5b9a8bb9c99baab55d77e33ee5dd8
Antony Gordon <gordonad@umich.edu>
2020-12-18 16:29:30 -0500

    Correct Largest.java to pass tests

--- src/code/Largest.java
+++ src/code/Largest.java
@@ -4,9 +4,9 @@ public class Largest {
 	public Largest() {}
     public int largest(int[] list){
         int index;
-        int max =0;
-        for (index = 1; index < list.length-1; index++){
-            if (list[index] < max) {
+        int max =list[0];
+        for (index = 1; index < list.length; index++){
+            if (list[index] > max) {
                 max = list[index];
             }
         }
